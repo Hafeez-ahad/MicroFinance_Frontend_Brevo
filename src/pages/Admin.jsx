c389fb43484f289a8b6e9caa5c9d192d8417f094
Hafeez-ahad <itxhafeex252@gmail.com>
2025-02-24 21:17:56 +0500

    push push again

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import { getReq } from '../Api/axios'
-import toast from 'react-hot-toast'
+
 import AdminC from '../components/admincomponent/AdminC'
 
 const AdminPage = () => {
