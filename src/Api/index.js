451a1a47b31df1acd9c8c1196381fa0db9e4b27d
Hafeez-ahad <itxhafeex252@gmail.com>
2025-02-24 20:26:51 +0500

    base url changed

--- src/Api/index.js
+++ src/Api/index.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
+
 const apiClient = axios.create({
-    baseURL: 'http://localhost:8000',
+    baseURL: "https://micro-finance-backend.vercel.app",
     timeout :7000,
     headers : {
         "Content-Type" : 'application/json'
